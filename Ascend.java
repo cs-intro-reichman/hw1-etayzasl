420851843c18d028832efad7eecc5238559ce2cd
etayzasl <etay.zaslavsky@post.runi.ac.il>
2024-11-07 08:16:24 +0200

    Add files via upload

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,23 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+	    int lim = Integer.parseInt(args[0]);
+		int a = (int)(Math.random() * lim);
+		int b = (int)(Math.random() * lim);
+		int c = (int)(Math.random() * lim);
+		System.out.println(a + " " + b + " " + c);
+
+		int min1 = Math.min(a, b);
+		int min2 = Math.min(b, c);
+		int min = Math.min(min1, min2);
+
+		int max1 = Math.max(a, b);
+		int max2 = Math.max(b, c);
+		int max = Math.max(max1, max2);
+
+		int med = a + b + c - min - max; 
+
+		System.out.println(min + " " + med + " " + max);
+	
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,12 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int number = Integer.parseInt(args[0]);
+		int hundreds, tens, ones = 0;
+	
+		hundreds = number / 100 % 10;
+		tens = number / 10 % 10;
+		ones = number % 10;
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 	}
 }
